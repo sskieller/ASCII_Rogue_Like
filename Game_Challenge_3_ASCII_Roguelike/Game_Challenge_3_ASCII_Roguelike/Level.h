9be021e658a8edf6f1498bc86658c21901e3da8c
Søren Skieller <soren.skieller@gmail.com>
2017-03-21 13:30:03 +0100

    level load, save, print

--- Game_Challenge_3_ASCII_Roguelike/Game_Challenge_3_ASCII_Roguelike/Level.h
+++ Game_Challenge_3_ASCII_Roguelike/Game_Challenge_3_ASCII_Roguelike/Level.h
@@ -1,10 +1,26 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <vector>
+
+using namespace std;
+
 class Level
 {
 public:
 	Level();
 
+	void loadLevel();
+	void clearLevel();
+	void saveLevel();
+	void printLevel();
 
-
+private:
+	ofstream outputFile;
+	ifstream inputFile;
+	vector<string> levelStrings;
+	string input;
+	string output;
 };
 
--- Game_Challenge_3_ASCII_Roguelike/Game_Challenge_3_ASCII_Roguelike/main.cpp
+++ Game_Challenge_3_ASCII_Roguelike/Game_Challenge_3_ASCII_Roguelike/main.cpp
@@ -3,13 +3,17 @@
 #include <conio.h> // _getch() for input
 #include <fstream> // Save to file
 
+#include "Level.h"
+
 using namespace std;
 
 
 int main()
 {
-
-	
+	Level level1;
+	level1.loadLevel();
+	level1.printLevel();
+	//level1.saveLevel();
 
 
 	system("PAUSE");
